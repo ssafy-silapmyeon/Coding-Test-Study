26abbecb16ed7531eabba95c28ad26091da08c54
holizon9 <holizon9@naver.com>
2024-01-05 21:07:49 +0900

    feat: BOJ_14501_퇴사 알고리즘 구현

---
+++ Park-Jaehyeon98/4주차/BOJ_14501_퇴사.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_14501_퇴사 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+
+        int[] t = new int[n];
+        int[] p = new int[n];
+
+        StringTokenizer st;
+        for (int i=0; i<n; i++) {
+            st = new StringTokenizer(reader.readLine());
+
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n+1];
+
+        for (int i=0; i<n; i++) {
+            if (i + t[i] <= n) {
+                dp[i + t[i]] = Math.max(dp[i + t[i]], dp[i] + p[i]);
+            }
+            dp[i+1] = Math.max(dp[i+1], dp[i]);
+        }
+        System.out.println(dp[n]);
+
+    }
+}
